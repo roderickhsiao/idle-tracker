f4aff5f722aeaf4055cf8717494b093a647b243f
Roderick Hsiao <roderickhsiao@gmail.com>
2019-02-09 16:21:32 -0800

    Fix CI test

--- test/idle-tracker.spec.js
+++ test/idle-tracker.spec.js
@@ -2,17 +2,10 @@ const ip = require('ip');
 
 describe('Idle tracker tests', () => {
   const server = `http://${ip.address()}:8080/test`;
-  let page;
-
-  before(async () => {
-    page = await browser.newPage();
-  });
-
-  after(async () => {
-    await page.close();
-  });
 
   it('page should become inactive after timeout', async () => {
+    const page = await browser.newPage();
+
     await page.goto(`${server}/test-case-inactive.html`);
     await page.waitFor(1000);
     const isActive = await page.evaluate(() => window.isActive);
@@ -20,11 +13,14 @@ describe('Idle tracker tests', () => {
   });
 
   it('page should become active after active events', async () => {
+    const page = await browser.newPage();
+
     await page.goto(`${server}/test-case-inactive.html`);
     await page.waitFor(1000);
     // mouse move
     await page.mouse.move(100, 100);
     const isActive = await page.evaluate(() => window.isActive);
     expect(isActive).to.be.true;
+    await page.close();
   });
 });
