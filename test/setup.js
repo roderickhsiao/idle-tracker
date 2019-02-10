50869eb7e14a2225016c4b1f09d07b05fd1bb1c1
Roderick Hsiao <roderickhsiao@gmail.com>
2019-02-09 16:29:11 -0800

    Revert test

--- test/setup.js
+++ test/setup.js
@@ -7,18 +7,15 @@ const globalVariables = pick(global, ['browser', 'expect']);
 // puppeteer options
 const opts = {
   headless: true,
-  slowMo: 100,
   timeout: 20000,
   args: ['--no-sandbox', '--disable-setuid-sandbox']
 };
 
-// expose variables
 before(async () => {
   global.expect = expect;
   global.browser = await puppeteer.launch(opts);
 });
 
-// close browser and reset global variables
 after(() => {
   browser.close();
 
