81714c73f74319aedb49193e7d621500ee9251cd
Roderick Hsiao <roderickhsiao@gmail.com>
2019-02-09 16:05:35 -0800

    Add functional tests

--- test/idle-tracker.spec.js
+++ test/idle-tracker.spec.js
@@ -1,25 +1,38 @@
 const ip = require('ip');
 
-describe('Idle tracker tests', function () {
+describe('Idle tracker tests', () => {
   const server = `http://${ip.address()}:8080/test`;
   let page;
 
-  before(async function () {
+  before(async () => {
     page = await browser.newPage();
   });
 
-  after(async function () {
+  after(async () => {
     await page.close();
   });
 
-  it('page should become inactive after timeout', async function (done) {
+  it('page should become inactive after timeout', async () => {
     const responseURLs = [];
     page.on('response', resp => {
       responseURLs.push(resp.url());
     });
     await page.goto(`${server}/test-case-inactive.html`);
     await page.waitFor(1000);
-    expect(isActive).to.be(false);
-    done();
+    let isActive = await page.evaluate(() => window.isActive);
+    expect(isActive).to.be.false;
+  });
+
+  it('page should become active after active events', async () => {
+    const responseURLs = [];
+    page.on('response', resp => {
+      responseURLs.push(resp.url());
+    });
+    await page.goto(`${server}/test-case-inactive.html`);
+    await page.waitFor(1000);
+    // mouse move
+    await page.mouse.move(100, 100);
+    isActive = await page.evaluate(() => window.isActive);
+    expect(isActive).to.be.true;
   });
 });
