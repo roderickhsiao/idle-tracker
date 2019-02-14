d918fb8ff76be950b5efa38285e7b1c5f53e6f67
Roderick Hsiao <roderickhsiao@gmail.com>
2019-02-14 15:49:09 -0800

    reduce timeout

--- test/idle-tracker.spec.js
+++ test/idle-tracker.spec.js
@@ -18,7 +18,7 @@ describe('Idle tracker tests', () => {
     let isActive = await page.evaluate(() => window.isActive);
     expect(isActive).to.be.false;
     await page.mouse.move(100, 100);
-    await page.waitFor(100);
+    await page.waitFor(50);
     isActive = await page.evaluate(() => window.isActive);
     expect(isActive).to.be.true;
   });
