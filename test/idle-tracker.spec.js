b257f856ca117437a0e6c0262954de24baa54ef3
Roderick Hsiao <roderickhsiao@gmail.com>
2019-02-14 16:06:37 -0800

    try new way to tes

--- test/idle-tracker.spec.js
+++ test/idle-tracker.spec.js
@@ -15,11 +15,17 @@ describe('Idle tracker tests', () => {
   it('page should become inactive after timeout and reactivate after events', async () => {
     await page.goto(`${server}/test-case-inactive.html`);
     await page.waitFor(1000);
-    let isActive = await page.evaluate(() => window.isActive);
-    expect(isActive).to.be.false;
+    let isActive = await page.evaluate(() => {
+      const el = document.getElementById('active-status');
+      return el.innerHTML;
+    });
+    expect(isActive).to.be('false');
     await page.mouse.move(100, 100);
     await page.waitFor(50);
-    isActive = await page.evaluate(() => window.isActive);
-    expect(isActive).to.be.true;
+    let isActive = await page.evaluate(() => {
+      const el = document.getElementById('active-status');
+      return el.innerHTML;
+    });
+    expect(isActive).to.be('true');
   });
 });
