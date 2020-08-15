e91ec68683b0e5a512f68f561f4138a92f4055d5
Roderick Hsiao <roderickhsiao@users.noreply.github.com>
2020-08-15 12:21:57 -0700

    Create supportPassiveEvent.js

---
+++ src/supportPassiveEvent.js
@@ -0,0 +1,15 @@
+let passiveSupported = false; // eslint-disable-line import/no-mutable-exports
+
+if (typeof window !== 'undefined') {
+  try {
+    const options = Object.defineProperty({}, 'passive', {
+      get() {
+        passiveSupported = true;
+      }
+    });
+
+    window.addEventListener('test', null, options);
+  } catch (err) {} // eslint-disable-line no-empty
+}
+
+export default passiveSupported;
